52d8d21a9fa7dec5ea242aaff5bf62d3d0824bf6
Favour Idowu <favouridowu02@gmail.com>
2025-09-15 15:20:02 +0100

    update: ..

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -65,3 +65,15 @@ function executeWork(employee: Director | Teacher): string {
     return employee.workTeacherTasks();
   }
 };
+
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  }
+  if (todayClass === "History") {
+    return "Teaching History";
+  }
+};
